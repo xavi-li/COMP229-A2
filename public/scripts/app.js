afc4883c9ff8d624c2401332306306771b049b62
Xavier Li <xavierli4ca@gmail.com>
2023-01-25 09:23:03 -0500

    Initial commit: Basic setup for EJS

---
+++ public/scripts/app.js
@@ -0,0 +1,6 @@
+(function () {
+    function start() {
+        console.log("App Started from /public/scripts/app.js ...")
+    }
+    window.addEventListener("load", start);
+}) ();
---
+++ routes/index.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET Home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Home' });
+});
+
+/* GET About Me page. */
+router.get('/about', function(req, res, next) {
+  res.render('index', { title: 'About Me' });
+});
+
+/* GET Projects page. */
+router.get('/projects', function(req, res, next) {
+  res.render('index', { title: 'Projects' });
+});
+
+/* GET Services page. */
+router.get('/services', function(req, res, next) {
+  res.render('index', { title: 'Services' });
+});
+
+/* GET Contact Me page. */
+router.get('/contact', function(req, res, next) {
+  res.render('index', { title: 'Contact Me' });
+});
+
+module.exports = router;
