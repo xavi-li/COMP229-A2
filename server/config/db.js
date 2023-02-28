85aef3c326eadf9dd06a8fe860a91bfcd127c773
Xavier Li <xavierli4ca@gmail.com>
2023-02-27 21:43:45 -0500

    Add CosmoDB URI

--- server/config/db.js
+++ server/config/db.js
@@ -1,7 +1,7 @@
 module.exports = {
 	// localhost
-	"URI":"mongodb://localhost/229a2"
+	//"URI":"mongodb://localhost/229a2"
 
 	// cloud
-	//"URI":"mongodb://xavi-li-mongodb:l5riVDxicNIMRYKAc1yc200CsiyiFAE50DTQBmghPRPdXElwJM2eXZEhtRROMo0hve4t7cs64DOwACDb0tF91A==@xavi-li-mongodb.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&maxIdleTimeMS=120000&appName=@xavi-li-mongodb@"
+	"URI": "mongodb://xavi-li-comp229-v2-server:CkgCy8cr4DumTo9nia8L755mtpN8VoFneITYw11irKpGRKW5OUoNboCbkjpfjZsd9pqifjKlOo9JACDbzGBzXw==@xavi-li-comp229-v2-server.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@xavi-li-comp229-v2-server@"
 }
